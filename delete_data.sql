39297cb33b3dda0d82b0b7659e3cdb31adbe8ae9
fazriegi <fazriegi80@gmail.com>
2023-11-09 13:24:15 +0700

    add deletion script

---
+++ delete_data.sql
@@ -0,0 +1,19 @@
+DELETE FROM product_categories;
+
+DELETE FROM categories; 
+
+DELETE FROM product_images;
+
+DELETE FROM order_items;
+
+DELETE FROM shippings;
+
+DELETE FROM payments;
+
+DELETE FROM orders;  
+
+DELETE FROM carts;
+
+DELETE FROM users;
+
+DELETE FROM products;
---
+++ delete_table.sql
@@ -0,0 +1,19 @@
+DROP TABLE product_categories;
+
+DROP TABLE categories; 
+
+DROP TABLE product_images;
+
+DROP TABLE order_items;
+
+DROP TABLE shippings;
+
+DROP TABLE payments;
+
+DROP TABLE orders;  
+
+DROP TABLE carts;
+
+DROP TABLE users;
+
+DROP TABLE products;
